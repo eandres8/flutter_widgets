f6c4fa442cbaa9f00a22abb463ca61b6184c6a97
Andres Vega <elb.andres8@gmail.com>
2025-08-11 17:02:19 -0500

    feat: create initial menu items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title, 
+    required this.subTitle, 
+    required this.link, 
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  
+  MenuItem(
+    title: 'Buttons',
+    subTitle: 'Several Flutter buttons',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  
+  MenuItem(
+    title: 'Cards',
+    subTitle: 'An styled container',
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];
